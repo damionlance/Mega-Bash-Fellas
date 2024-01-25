e73664635fce796b2e5c7b85d43a64805dae2e04
David Hartman <djhartman1216@gmail.com>
2024-01-25 14:40:07 -0500

    Animation stuff

---
+++ scripts/tools/character import.gd
@@ -0,0 +1,19 @@
+@tool
+extends EditorScenePostImport
+
+
+# Called when the node enters the scene tree for the first time.
+func _post_import(scene):
+	
+	iterate(scene)
+
+func iterate(node):
+	if node.name == "AnimationPlayer":
+		set_save_custom_tracks(node)
+	for child in node.get_children():
+		iterate(child)
+
+func set_save_custom_tracks(node):
+	var animation_names = node.get_animation_list()
+	for name in animation_names:
+		print(node.get_animation(name).get_property_list())
